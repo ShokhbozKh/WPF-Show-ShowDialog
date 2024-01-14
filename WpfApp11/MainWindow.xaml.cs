b3180f4dd31321e51ecd72f09dae8fa0d4eade50
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2024-01-15 00:43:23 +0500

    Add project files.

---
+++ WpfApp11/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp11.View;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            NormalWindow normalWindow = new NormalWindow(this);
+
+            normalWindow.Show();
+        }
+
+        private void btnModal_Click(object sender, RoutedEventArgs e)
+        {
+            ModalWindow modalWindow = new ModalWindow(this);
+            Opacity = 0.4;
+            modalWindow.ShowDialog();
+            Opacity = 1;// opacity  = 1 qilmasa avvalgi eski xolida qolib ketadi
+            if (modalWindow.Success)
+            {
+                txtInput.Text = modalWindow.Input;
+            }
+        }
+    }
+}
---
+++ WpfApp11/View/ModalWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp11.View
+{
+    /// <summary>
+    /// Interaction logic for ModalWindow.xaml
+    /// </summary>
+    public partial class ModalWindow : Window
+    {
+        public bool Success { get; set; }
+        public string Input { get; set; }
+        public ModalWindow(Window window)
+        {
+            Owner = window;
+            InitializeComponent();
+        }
+        private void btnNormal_Ok(object sender, RoutedEventArgs e)
+        {
+            Success = true;
+            Input = txtInput.Text;
+            Close();
+
+        }
+
+        private void btnModal_Cencel(object sender, RoutedEventArgs e)
+        {
+            Success = false;
+            Close();
+        }
+
+        private void txtInput_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtInput.Text))
+            {
+                btnOk.IsEnabled = true;
+            }
+            else { btnOk.IsEnabled = false; }
+
+        }
+    }
+}
